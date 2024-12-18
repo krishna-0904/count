bbf6cd2f1e840e3423baad485c37ff887235d6c4
krishna-0904 <krishnaprabhas601@gmail.com>
2024-12-18 08:49:34 +0530

    Update count.py

--- count.py
+++ count.py
@@ -1,6 +1,23 @@
+# count=0
+# val='rama'
+# for x in val:
+#     print(x)
+#     count=count+1
+#     print(count)
+# #print(count)
+   
+
+# count=0
+# for x in range(1,10,2):
+#     print(x)
+#     count=count+1
+# print("total values of odd:",count)
+
 count=0
-val='rama'
-for x in val:
+sum=0
+for x in range(1,10,2):
     print(x)
     count=count+1
-    print(count)
+    sum=sum+x
+print("total values of odd:",count)
+print("total sum of odd:",sum)
